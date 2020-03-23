2f19d5f4dfc47cf435d51568be368fc5d18ba513
s.porfiri <s.porfiri@gmail.com>
2020-03-23 19:29:24 +0100

    esercizi del 23/03/2020

---
+++ Booleani/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Booleani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //AND logico
+            int numero1 = 35;
+            int numero2 = 60;
+            bool risultato1 = numero1 == 35;
+            bool risultato2 = numero2 > 50;
+            Console.WriteLine("stampa il risultato dell'AND logico {0}", risultato1 && risultato2);
+
+            risultato1 = numero1 > 0;
+            risultato2 = numero2 < 50;
+            Console.WriteLine("stampa il risultato dell'AND logico {0}", risultato1 && risultato2);
+
+            //OR logico
+            risultato1 = numero1 < 35;
+            risultato2 = numero2 < 50;
+            Console.WriteLine("stampa il risultato dell'OR logico {0}", risultato1 || risultato2);
+
+            Console.ReadLine();
+        }
+    }
+}
--- Variabili/Program.cs
+++ Variabili/Program.cs
@@ -10,6 +10,29 @@ namespace Variabili
     {
         static void Main(string[] args)
         {
+            //definizione(assegnamento) di una variabile intera
+            int età = 30;
+            
+            //definizione di due varabili intere e loro somma 
+            int add1 = 10;
+            int add2 = 25;
+            int sum = add1 + add2;
+            Console.WriteLine("La somma degli addendi è {0}", sum);
+
+            //utilizzo del cast per fare una divsione non intera
+            double divisione = ((double)add1 / (double)add2 ) * 2;
+            Console.WriteLine("La divione dei dividendi è {0}", divisione);
+
+            //untlizzo della class Math per il calcolo della radice quadrata
+            double numero = 256;
+            Console.WriteLine("radice quadra di {0} è: {1}",numero, Math.Sqrt(numero));
+         
+            //esempio di cast con perdita della precisione
+            double pippo = 3.14;
+            Console.WriteLine("stampa la variabile pippo = {0}", (int)pippo);
+
+            Console.ReadLine();
+
         }
 
     }
