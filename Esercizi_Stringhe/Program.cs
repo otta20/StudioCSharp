945f15dce8d91600418c2ddd8984b05c1c6339dc
s.porfiri <s.porfiri@gmail.com>
2020-04-10 21:23:19 +0200

    esercizi 7-9

--- Esercizi_Stringhe/Program.cs
+++ Esercizi_Stringhe/Program.cs
@@ -15,7 +15,10 @@ namespace Esercizi_Stringhe
             //Esercizio6();
             //Esercizio1();
             //Esercizio2();
-            Esercizio3();
+            //Esercizio3();
+            //Esercizio7();
+            //Esercizio8();
+            Esercizio9();
             Console.ReadLine();
         }
         static void Esercizio4()
@@ -42,21 +45,22 @@ namespace Esercizi_Stringhe
             Console.WriteLine($"{fraseFinale}");
         }
 
-        static void Esercizio5()
-        {
-            //Write a program in C# Sharp to count the total number of words in a string
-            Console.WriteLine("### ESERCIZIO 5 ###");
-            Console.WriteLine("Scrivi un programma in C# che conteggi il numero delle parole in una stringa");
-            Console.WriteLine("Inserisci la tua frase");
-            string fraseInserita = Console.ReadLine();
-            char[] caratteriDaEliminare = new char[] { ' ', ',', ';', ':', '!', '?', '^', '=', ')', '(', '/', '%', '£', '"', '|', '\n', '{', '}', '[', ']' };
+        /*static void Esercizio5()
+{
+    //Write a program in C# Sharp to count the total number of words in a string
+    Console.WriteLine("### ESERC
+IZIO 5 ###");
+    Console.WriteLine("Scrivi un programma in C# che conteggi il numero delle parole in una stringa");
+    Console.WriteLine("Inserisci la tua frase");
+    string fraseInserita = Console.ReadLine();
+    char[] caratteriDaEliminare = new char[] { ' ', ',', ';', ':', '!', '?', '^', '=', ')', '(', '/', '%', '£', '"', '|', '\n', '{', '}', '[', ']' };
 
-            string[] fraseDaManipolare = fraseInserita.Split(caratteriDaEliminare, StringSplitOptions.None);
+    string[] fraseDaManipolare = fraseInserita.Split(caratteriDaEliminare, StringSplitOptions.None);
 
-            Console.WriteLine($"La frase che hai inserito {fraseInserita} contiene n.{fraseDaManipolare.Length} stringhe");
-        }
+    Console.WriteLine($"La frase che hai inserito {fraseInserita} contiene n.{fraseDaManipolare.Length} stringhe");
+}*/
 
-        static void Esercizio6()
+        /*static void Esercizio6()
         {
             Console.WriteLine("Write a program in C# Sharp to compare two string without using string library functions");
             Console.WriteLine("Inserisci la prima frase: ");
@@ -90,32 +94,32 @@ namespace Esercizi_Stringhe
                   Console.WriteLine("le due frasi inserite non sono uguali");
               }
               */
-            if (parolePrimaFrase.Length != paroleSecondaFrase.Length)
+        /*if (parolePrimaFrase.Length != paroleSecondaFrase.Length)
+        {
+            Console.WriteLine("le due frasi inserite non sono uguali");
+        }
+        else
+        {
+            bool leFrasiSonoUguali = true;
+            for (int p = 0; p < parolePrimaFrase.Length - 1; p++)
             {
-                Console.WriteLine("le due frasi inserite non sono uguali");
+                if (paroleSecondaFrase[p] != parolePrimaFrase[p])
+                {
+                    leFrasiSonoUguali = false; 
+                    break;
+                }
+            }
+            if (leFrasiSonoUguali)
+            {
+                Console.WriteLine("Le due frasi hanno uguale lunghezza e le loro parole sono disposte nello stesso ordine.");
             }
             else
             {
-                bool leFrasiSonoUguali = true;
-                for (int p = 0; p < parolePrimaFrase.Length - 1; p++)
-                {
-                    if (paroleSecondaFrase[p] != parolePrimaFrase[p])
-                    {
-                        leFrasiSonoUguali = false; 
-                        break;
-                    }
-                }
-                if (leFrasiSonoUguali)
-                {
-                    Console.WriteLine("Le due frasi hanno uguale lunghezza e le loro parole sono disposte nello stesso ordine.");
-                }
-                else
-                {
-                    Console.WriteLine("Le due frasi hanno uguale lunghezza , ma le parole al loro interno non corrispondono.");
-                }
-                
+                Console.WriteLine("Le due frasi hanno uguale lunghezza , ma le parole al loro interno non corrispondono.");
             }
+
         }
+    }*/
 
         static void Esercizio1()
         {
@@ -125,7 +129,7 @@ namespace Esercizi_Stringhe
             int d;
 
             Console.WriteLine("Inserisci qui i numeri dell'array: ");
-            for (d = 0; d < 10; d++ )
+            for (d = 0; d < 10; d++)
             {
                 Console.Write($"numero - {d}: ");
                 dieciPosizioni[d] = Convert.ToInt32(Console.ReadLine());
@@ -164,19 +168,146 @@ namespace Esercizi_Stringhe
             Console.Write($"inserisci il numenro degli elementi del tuo array: ");
             int n = Convert.ToInt32(Console.ReadLine());
             int[] elencoInserito = new int[n];
-            for (int e = 0; e <= n-1; e++)
+            for (int e = 0; e <= n - 1; e++)
             {
                 Console.Write($"elemento {e}: ");
-                elencoInserito[e]  = Convert.ToInt32(Console.ReadLine());
+                elencoInserito[e] = Convert.ToInt32(Console.ReadLine());
                 sum = sum + elencoInserito[e];
             }
-            Console.WriteLine($"La somma degli elementi inseriti è: {sum}");            
+            Console.WriteLine($"La somma degli elementi inseriti è: {sum}");
 
         }
 
-        static void Esercizio()
+        static void Esercizio5()
         {
+            Console.WriteLine("Write a program in C# Sharp to count a total number of duplicate elements in an array.");
+            Console.WriteLine("inserisci l'indice del tuo array: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int conteggioNumeriDuplicati = 0;
+            int[] elencoNumeri = new int[n];
+            int[] elencoCopiato = new int[n];
 
+            Console.WriteLine("Inserisci gli elementi del tuo array: ");
+            for (int e = 0; e <= n - 1; e++)
+            {
+                Console.Write($"elemento {e}: ");
+                elencoNumeri[e] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int e = 0; e <= n - 1; e++)
+            {
+                for (int d = e + 1; d <= n - 1; d++)
+                {
+                    if (elencoNumeri[e] == elencoNumeri[d])
+                    {
+                        conteggioNumeriDuplicati++;
+                    }
+                }
+            }
+            Console.WriteLine($"l'elenco inserito contiene n.{conteggioNumeriDuplicati} numeri uguali");
+        }
+
+        static void Esercizio6()
+        {
+            Console.WriteLine("Write a program in C# Sharp to print all unique elements in an array");
+            Console.WriteLine("Inserisci la grandezza dell'array: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] elencoNumeriInseriti = new int[n];
+            Console.WriteLine("inserisci gli elementi dell'array: ");
+            for (int e = 0; e <= n - 1; e++)
+            {
+                Console.Write($"elemento - {e}: ");
+                elencoNumeriInseriti[e] = Convert.ToInt32(Console.ReadLine());
+            }
+            int u = 0;
+            int[] elencoNumeriUnici = new int[u];
+            for (int e = 0; e <= n - 1; e++)
+            {
+                for (int d = e + 1; d <= n - 1; d++)
+                {
+                    if (elencoNumeriInseriti[e] != elencoNumeriInseriti[d])
+                    {
+                        u++;
+                    }
+                }
+            }
+            Console.WriteLine($"ci sono n.{u} numeri unici e sono questi");
+
+        }
+
+        static void Esercizio7()
+        {
+            Console.WriteLine("Write a program in C# Sharp to count a total number of alphabets, digits and special characters");
+            Console.WriteLine("Scrivi qui la tua frase: ");
+            string fraseInserita = Console.ReadLine();
+            char[] elencoNumeri = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            char[] elencoLettere = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'x', 'z', 'è', 'é', 'ì', 'ò', 'à', 'ù' };
+            char[] caratteriSpeciali = new char[] { '@', '§', '#', '£', '$', '&' };
+
+
+            int conteggioNumeri = 0;
+            int conteggioLettere = 0;
+            int conteggioSpeciali = 0;
+            for (int c = 0; c <= fraseInserita.Length - 1; c++)
+            {
+                if (fraseInserita[c] >= '0' && fraseInserita[c] <= '9')
+                {
+                    conteggioNumeri++;
+                }
+                else if (fraseInserita[c] >= 'a' && fraseInserita[c] <= 'ù')
+                {
+                    conteggioLettere++;
+                }
+                else
+                {
+                    conteggioSpeciali++;
+                }
+            }
+            Console.WriteLine($"nella frase che hai inserito {fraseInserita} ci sono n.{conteggioNumeri} numeri, n.{conteggioLettere} lettere e n.{conteggioSpeciali} caratteri speciali.");
+
+        }
+
+        static void Esercizio8()
+        {
+            Console.WriteLine("Esercizio 8: Write a program in C# Sharp to copy one string to another string");
+            Console.WriteLine("Inserisci qui la tua frase: ");
+            string fraseInserita = Console.ReadLine();
+
+            char[] fraseDuplicata = new char[fraseInserita.Length];
+           // string fraseFinale = "";
+            for (int f = 0; f <= fraseInserita.Length-1; f++)
+            {
+                fraseDuplicata[f] = fraseInserita[f];
+               // fraseFinale = fraseFinale + fraseDuplicata[f];
+            }
+            Console.Write($"la frase che hai inserito è: {fraseInserita},\nquella duplicata è: {string.Join("",fraseDuplicata)}.\nIl numero dei caratteri copiati è {fraseInserita.Length}");
+        }
+
+        static void Esercizio9()
+        {
+            //Write a program in C# Sharp to count a total number of vowel or consonant in a string
+            Console.WriteLine("Inserisci ui la tua frase: ");
+            string fraseInserita = Console.ReadLine();
+            int conteggioVocali = 0;
+            int conteggioConsonanti = 0;
+            char[] elencoVocali = new char[] {'a','e','i','o','u','ì','è','é','ò','à','ù'};
+
+            bool contieneVocali = false;
+
+            for (int f = 0; f <= fraseInserita.Length-1; f++)
+            {
+                if( fraseInserita[f] == 'a' || fraseInserita[f] == 'e' || fraseInserita[f] == 'i' || fraseInserita[f] == 'o' || fraseInserita[f] == 'u' || fraseInserita[f] == 'ì' || fraseInserita[f] == 'è' || fraseInserita[f] == 'é' || fraseInserita[f] == 'ò' || fraseInserita[f] == 'à' || fraseInserita[f] == 'ù')
+                {
+                        conteggioVocali++;
+                }
+                else if (fraseInserita[f] >= 'b' && fraseInserita[f] <= 'z' )
+                {
+                    conteggioConsonanti++;
+                }
+            }
+            Console.WriteLine($"la frse inserita contine n.{conteggioVocali} vocali e n.{conteggioConsonanti} consonanti");
+        }
+        static void Esercizio10()
+        {
         }
     }
 }
