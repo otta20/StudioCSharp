c82f932e2cd09d136c6a6b33509a82533627a0b5
Marco Ciaschini <mciasco@libero.it>
2020-04-28 17:37:26 +0200

    Aggiunto progetto MorraCinese come esercizio sulla suddivisione di un programma in funzioni

---
+++ MorraCinese/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Console = System.Console;
+
+namespace MorraCinese
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        
+        // Funzioni date
+        static bool IsEqualToNumber(string stringToVerify, int targetNumber)
+        {
+            int parsedInt;
+            return int.TryParse(stringToVerify, out parsedInt) &&
+                   parsedInt == targetNumber;
+        }
+        
+        private static int GetRandomNumber(int minInclusive, int maxInclusive)
+        {
+            Random random = new Random(DateTime.Now.Millisecond);
+            return random.Next(minInclusive, maxInclusive + 1);
+        }
+    }
+}
